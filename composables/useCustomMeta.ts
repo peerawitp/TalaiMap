67e816cc97a8af32569444f8132395e62f16dd45
Peerawit P <jawzainw55@hotmail.com>
2022-09-30 22:47:23 +0700

    🖼 Added metaimage

--- composables/useCustomMeta.ts
+++ composables/useCustomMeta.ts
@@ -1,7 +1,8 @@
 export const useCustomMeta = () => {
   let defaultDesc = 'KU TalaiMap - Talai realtime tracking'
+  let defaultImage = '/og.png'
 
-  const setMeta = (title: string, desc = defaultDesc) => {
+  const setMeta = (title: string, desc = defaultDesc, image = defaultImage) => {
     useMeta({
       title,
       meta: [
@@ -13,6 +14,7 @@ export const useCustomMeta = () => {
         { name: 'og:description', content: desc },
         { name: 'og:type', content: 'website' },
         { name: 'og:url', content: 'https://talaimap.vercel.app/' },
+        { name: 'og:image', content: image },
       ],
       link: [{ rel: 'icon', type: 'image/png', href: '/favicon.png' }],
     })
