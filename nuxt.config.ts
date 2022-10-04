0af58e47ac4963eb238f1e0345bcfd76b1185f8e
Peerawit P <jawzainw55@hotmail.com>
2022-10-04 18:06:47 +0700

    💨 Migrate from Windi to Tailwind due to daisyUI

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ import { defineNuxtConfig } from 'nuxt'
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
   css: ['@/assets/styles.css', 'leaflet/dist/leaflet.css'],
-  buildModules: ['nuxt-windicss', 'unplugin-icons/nuxt'],
+  buildModules: ['@nuxtjs/tailwindcss', 'unplugin-icons/nuxt'],
   publicRuntimeConfig: {
     MAP_API_KEY: process.env.MAP_API_KEY,
   },
--- windi.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'windicss/helpers'
-export default defineConfig({
-  plugins: [
-    require('daisyui'),
-    require('windicss/plugin/typography'),
-    require('windicss/plugin/aspect-ratio'),
-  ],
-})
